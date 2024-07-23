5497a12f2606ad68bcb4bbe03cc32431c3551293
demandtech <demandwork247@gmail.com>
2024-07-23 22:20:10 +0100

    fix: add abd delete fix

--- src/components/linkpage/LinkCard.tsx
+++ src/components/linkpage/LinkCard.tsx
@@ -1,121 +1,163 @@
-import React, { useState } from "react";
+import { useEffect, useState, FC, FocusEventHandler, ChangeEvent } from "react";
 import {
-  AnchorIcon,
-  EqualIcon,
-  GithubIcon,
-  FacebookIcon,
-  LinkedinIcon,
-  FrontendMentorIcon,
-  YoutubeIcon,
+	AnchorIcon,
+	EqualIcon,
+	GithubIcon,
+	FacebookIcon,
+	LinkedinIcon,
+	FrontendMentorIcon,
+	YoutubeIcon,
 } from "../Svgs";
 import { Button, Input } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
 import { LinkCardProps } from "@/types";
 import { useAppContext } from "@/app/contexts/AppContext";
 
-const LinkCard: React.FC<LinkCardProps> = ({ index }) => {
-  const [value, setValue] = React.useState({
-    name: "",
-    url: "",
-    icon: "",
-  });
-  const { links, updateLinks } = useAppContext();
+const LinkCard: FC<LinkCardProps> = ({ index, link }) => {
+	const [value, setValue] = useState(link.name);
+	const [url, setUrl] = useState(link.url);
+	const { links, updateLinks } = useAppContext();
 
-  const handleSelectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setValue((prev) => {
-      return {
-        ...prev,
-      };
-    });
-  };
+	const handleSelectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+		const selectedValue = e.target.value;
 
-  const platforms: Array<{ name: string; icon: React.ReactNode }> = [
-    {
-      name: "Github",
-      icon: <GithubIcon />,
-    },
-    {
-      name: "Facebook",
-      icon: <FacebookIcon />,
-    },
-    {
-      name: "Frontend Mentor",
-      icon: <FrontendMentorIcon />,
-    },
-    {
-      name: "Youtube",
-      icon: <YoutubeIcon />,
-    },
-    {
-      name: "Linkedin",
-      icon: <LinkedinIcon />,
-    },
-  ];
+		setValue(selectedValue);
 
-  const handlRemoveLink = () => {
-    const newLinks = links.filter((_, idx) => idx !== index);
-    updateLinks(newLinks);
-  };
+		const updatedLinks = links.map((li, idx) =>
+			li.name === link.name
+				? {
+						...li,
+						name: selectedValue,
+				  }
+				: li
+		);
 
-  return (
-    <div className="bg-lightGrey rounded-xl p-5">
-      <div className="flex justify-between items-center mb-3">
-        <div className="flex items-center gap-1.5">
-          <EqualIcon />
-          <p className="font-bold text-grey">Link #{index + 1}</p>
-        </div>
-        <Button
-          className="text-grey font-normal px-0 justify-end"
-          variant="light"
-          onPress={handlRemoveLink}
-        >
-          Remove
-        </Button>
-      </div>
-      <div className="flex flex-col space-y-3">
-        <div className="">
-          <Select
-            labelPlacement="outside"
-            label="Platform"
-            className="w-full rounded-lg"
-            variant="bordered"
-            radius="sm"
-            placeholder="Select platform"
-            classNames={{
-              mainWrapper: "",
-              innerWrapper: "rounded-sm ",
-              trigger: "border-1 focus:shadow-inputHover",
-            }}
-            onChange={handleSelectionChange}
-            selectedKeys={[value]}
-            startContent={value !== "" && platforms[Number(value)]?.icon}
-          >
-            {platforms.map((platform, index) => (
-              <SelectItem startContent={platform.icon} key={index}>
-                {platform.name}
-              </SelectItem>
-            ))}
-          </Select>
-        </div>
-        <div>
-          <Input
-            startContent={<AnchorIcon className="fill-grey" />}
-            variant="bordered"
-            color="primary"
-            label="Link"
-            placeholder="e.g. https://www.github.com/johnappleseed"
-            labelPlacement="outside"
-            classNames={{
-              label: "text-darkGrey",
-              inputWrapper:
-                "rounded-lg hover:border-primary hover:shadow-inputHover border-1 focus-within:shadow-inputHover",
-            }}
-            className="hover:border-primary"
-          />
-        </div>
-      </div>
-    </div>
-  );
+		updateLinks(updatedLinks);
+	};
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setUrl(e.target.value);
+		const newUrl = e.target.value;
+
+		const updatedLinks = links.map((li, idx) =>
+			li.name === link.name ? { ...li, url: newUrl } : li
+		);
+
+		updateLinks(updatedLinks);
+	};
+
+	const handleInputBlur = (e: FocusEventHandler<HTMLInputElement>) => {
+		console.log(e);
+		const newUrl = e.target.value;
+		setUrl(newUrl);
+
+		try {
+			new URL(newUrl);
+
+			setUrl(newUrl);
+		} catch (error) {
+			console.error("Invalid URL:", newUrl);
+
+			alert("Please enter a valid URL.");
+		}
+	};
+
+	const platforms: Array<{ name: string; icon: React.ReactNode; id: number }> =
+		[
+			{
+				id: 1,
+				name: "Github",
+				icon: <GithubIcon />,
+			},
+			{
+				id: 2,
+				name: "Facebook",
+				icon: <FacebookIcon />,
+			},
+			{
+				id: 3,
+				name: "Frontend Mentor",
+				icon: <FrontendMentorIcon />,
+			},
+			{
+				id: 4,
+				name: "Youtube",
+				icon: <YoutubeIcon />,
+			},
+			{
+				id: 5,
+				name: "Linkedin",
+				icon: <LinkedinIcon />,
+			},
+		];
+
+	const handlRemoveLink = () => {
+		const newLinks = links.filter((li) => li.name !== link.name);
+		updateLinks(newLinks);
+	};
+
+	return (
+		<div className="bg-lightGrey rounded-xl p-5">
+			<div className="flex justify-between items-center mb-3">
+				<div className="flex items-center gap-1.5">
+					<EqualIcon />
+					<p className="font-bold text-grey">Link #{index + 1}</p>
+				</div>
+				<Button
+					className="text-grey font-normal px-0 justify-end"
+					variant="light"
+					onPress={handlRemoveLink}
+				>
+					Remove
+				</Button>
+			</div>
+			<div className="flex flex-col space-y-3">
+				<div className="">
+					<Select
+						labelPlacement="outside"
+						label="Platform"
+						className="w-full rounded-lg"
+						variant="bordered"
+						radius="sm"
+						placeholder="Select platform"
+						classNames={{
+							mainWrapper: "",
+							innerWrapper: "rounded-sm ",
+							trigger: "border-1 focus:shadow-inputHover",
+						}}
+						onChange={handleSelectionChange}
+						selectedKeys={[value]}
+						startContent={platforms.find((plat) => plat.name == value)?.icon}
+					>
+						{platforms.map((platform) => (
+							<SelectItem startContent={platform.icon} key={platform.name}>
+								{platform.name}
+							</SelectItem>
+						))}
+					</Select>
+				</div>
+				<div>
+					<Input
+						startContent={<AnchorIcon className="fill-grey" />}
+						variant="bordered"
+						color="primary"
+						label="Link"
+						placeholder="e.g. https://www.github.com/johnappleseed"
+						labelPlacement="outside"
+						classNames={{
+							label: "text-darkGrey",
+							inputWrapper:
+								"rounded-lg hover:border-primary hover:shadow-inputHover border-1 focus-within:shadow-inputHover",
+						}}
+						className="hover:border-primary"
+						value={url}
+						onChange={handleInputChange}
+						onBlur={handleInputBlur}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LinkCard;
--- src/components/linkpage/Links.tsx
+++ src/components/linkpage/Links.tsx
@@ -3,14 +3,14 @@ import LinkCard from "./LinkCard";
 import { useAppContext } from "@/app/contexts/AppContext";
 
 const Links = () => {
-  const { updateLinks, links } = useAppContext();
-  return (
-    <div className="space-y-6">
-      {links.map((_, index) => {
-        return <LinkCard setLinks={updateLinks} index={index} key={index} />;
-      })}
-    </div>
-  );
+	const { links } = useAppContext();
+	return (
+		<div className="space-y-6">
+			{links.map((link, index) => {
+				return <LinkCard link={link} index={index} key={link.name} />;
+			})}
+		</div>
+	);
 };
 
 export default Links;
